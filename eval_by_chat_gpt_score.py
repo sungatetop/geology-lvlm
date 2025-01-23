4b53bbac3ef10784001719b2159e015799471973
atlas <903216099@qq.com>
2025-01-23 18:23:43 +0800

    更新评估脚本

--- eval_by_chat_gpt_score.py
+++ eval_by_chat_gpt_score.py
@@ -36,7 +36,6 @@ class GPTEvaluator:
                   f'[{ROLE} 1]\n{ans1}\n\n[End of {ROLE} 1]\n\n'
                   f'[{ROLE} 2]\n{ans2}\n\n[End of {ROLE} 2]\n\n'
                   f'[System]\n{INSTRUCT_PROMPT}\n\n')
-        print(content)
         return [
             {"role": "system", "content": "You are a helpful and precise assistant for checking the quality of the answer."},
             {"role": "user", "content": content}
@@ -109,12 +108,15 @@ async def main(args):
         api_key=args.api_key,
         base_url=args.base_url
     )
-    
+    already=os.listdir(args.output_dir)
+    already=[e.replace("_scores.jsonl","") for e in already]
     # 加载所有主题的LLaVA回答
     inferresult = {}
     for file_name in os.listdir(args.llava_dir):
         if file_name.endswith('.jsonl'):
             theme = file_name.split('.')[0]
+            if theme in already:
+                continue
             file_path = os.path.join(args.llava_dir, file_name)
             inferresult[theme] = load_theme_data(file_path)
             
@@ -126,49 +128,49 @@ async def main(args):
         for item in inferresult[theme]:
             id,data=process_conversations(item,theme)
             samples.extend(data)                
-    # 使用GPT-4进行评估
-    results = await evaluator.evaluate_batch(samples)
-    
-    # 保存结果
-    with open(args.scores_file, 'w', encoding='utf-8') as f:
-        for result in results:
-            f.write(json.dumps(result, ensure_ascii=False) + '\n')
-            
-    # 统计不同主题的得分
-    score_theme_dict = defaultdict(lambda: defaultdict(list))
-    for x in results:
-        theme = x['theme']
-        scores = x['gpt_eval'].split('\n')[0].split(' ')
-        if len(scores) >= 2:
-            llava_score = float(scores[0])
-            gpt_score = float(scores[1])
-            score_theme_dict[theme][1].append(llava_score)  # LLaVA scores
-            score_theme_dict[theme][2].append(gpt_score)    # GPT scores
-            score_theme_dict['overall'][1].append(llava_score)
-            score_theme_dict['overall'][2].append(gpt_score)
-    
-    # 计算各项统计指标
-    result = defaultdict(dict)
-    for theme, score_dict in score_theme_dict.items():
-        result[theme]['llava_score'] = util.get_avg(score_dict[1])
-        result[theme]['gpt_score'] = util.get_avg(score_dict[2])
-        result[theme]['relative_score'] = util.get_avg([float(s2)/float(s1) for s1, s2 in zip(score_dict[1], score_dict[2])])*100
-        result[theme]['data_size'] = len(score_dict[1])
+        # 使用GPT-4进行评估
+        results = await evaluator.evaluate_batch(samples)
         
-    # 输出结果
-    df = pd.DataFrame.from_dict(result).T
-    print("\nModel Performance Comparison by Theme:")
-    print(df)
-    
-    # 输出每个主题的样本数量
-    print("\nSample counts by theme:")
-    for theme in result:
-        print(f"{theme}: {result[theme]['data_size']}")
+        # 保存结果
+        with open(os.path.join(args.output_dir,theme+"_scores.jsonl"), 'w', encoding='utf-8') as f:
+            for result in results:
+                f.write(json.dumps(result, ensure_ascii=False) + '\n')
+                
+        # 统计不同主题的得分
+        score_theme_dict = defaultdict(lambda: defaultdict(list))
+        for x in results:
+            theme = x['theme']
+            scores = x['gpt_eval'].split('\n')[0].split(' ')
+            if len(scores) >= 2:
+                llava_score = float(scores[0])
+                gpt_score = float(scores[1])
+                score_theme_dict[theme][1].append(llava_score)  # LLaVA scores
+                score_theme_dict[theme][2].append(gpt_score)    # GPT scores
+                score_theme_dict['overall'][1].append(llava_score)
+                score_theme_dict['overall'][2].append(gpt_score)
+        
+        # 计算各项统计指标
+        result = defaultdict(dict)
+        for theme, score_dict in score_theme_dict.items():
+            result[theme]['llava_score'] = util.get_avg(score_dict[1])
+            result[theme]['gpt_score'] = util.get_avg(score_dict[2])
+            result[theme]['relative_score'] = util.get_avg([float(s2)/float(s1) for s1, s2 in zip(score_dict[1], score_dict[2])])*100
+            result[theme]['data_size'] = len(score_dict[1])
+            
+        # 输出结果
+        df = pd.DataFrame.from_dict(result).T
+        print("\nModel Performance Comparison by Theme:")
+        print(df)
+        
+        # 输出每个主题的样本数量
+        print("\nSample counts by theme:")
+        for theme in result:
+            print(f"{theme}: {result[theme]['data_size']}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("LLaVA vs GPT Model Comparison", add_help=True)
     parser.add_argument("--llava-dir", type=str, required=True, help="directory containing LLaVA answer files")
-    parser.add_argument("--scores-file", default="scores.jsonl", help="path to save score file")
+    parser.add_argument("--output-dir", default="", help="path to save score file")
     parser.add_argument("--api-key", required=False,default="", help="OpenAI API key")
     parser.add_argument("--base_url", required=False,default="https://api.deepseek.com", help="base_url")
     args = parser.parse_args()
